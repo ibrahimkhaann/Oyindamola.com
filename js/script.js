145cd7966a1b8a110d0185b3beb9156ddaffd413
ibrahimkkhaann <you@example.com>
2024-12-28 20:23:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+  //  Stykey Header  
+  $(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    
+    if(scrolling > 200){
+        $('.navbar').addClass('sticky_header');
+    }
+    else {
+         $('.navbar').removeClass('sticky_header');
+    }
+    });  
+    //==== Back-to-top button
+  $(window).on('scroll', function(event) {
+    if($(this).scrollTop() > 600){
+        $('.back-to-top').fadeIn(200)
+    } else{
+        $('.back-to-top').fadeOut(200)
+    }
+});
+//==== Animate the scroll to top
+$('.back-to-top').on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 0,
+    }, 1000);
+});
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
